c5ab09b4a704b811e4b66eb84f5f52386850508b
Robert812218 <robert812@shaw.ca>
2023-04-08 11:46:03 -0600

    add files

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@ use std::fs;
 use std::io::{BufReader, Read};
 use std::path::Path;
 
-// Define a struct to hold the title and content of a text file
 struct TextFile {
     title: String,
     content: String,
+    id: u8,
 }
 
-// Define a function to read the contents of a file given its path
 fn read_file(path: &Path) -> Result<String, std::io::Error> {
     let file = fs::File::open(path)?;
     let mut buf_reader = BufReader::new(file);
@@ -21,6 +20,8 @@ fn main() {
     let folder_path = Path::new("src/library");
     let mut text_files = Vec::new();
 
+    let mut id_count: u8 = 0;
+
     for entry in fs::read_dir(folder_path).unwrap() {
         let entry = entry.unwrap();
         let file_path = entry.path();
@@ -28,18 +29,33 @@ fn main() {
 
         if file_path.is_file() {
             if let Ok(content) = read_file(&file_path) {
+                id_count += 1;
                 let text_file = TextFile {
                     title: file_name,
                     content: content,
+                    id: id_count,
                 };
                 text_files.push(text_file);
             }
         }
     }
 
-    // Do something with the text_files vector
-    for text_file in text_files {
-        println!("Title: {}", text_file.title);
-        println!("Content: {}", text_file.content);
+    println!("Your library:");
+    for text_file in &text_files {
+        println!("Name: {}, id: {}", text_file.title, text_file.id);
+    }
+
+    let mut input = String::new();
+    println!("Enter the id of the file you want to read:");
+    std::io::stdin().read_line(&mut input).unwrap();
+
+    let selected_id = input.trim().parse::<u8>().unwrap();
+
+    let selected_file = text_files.iter().find(|f| f.id == selected_id);
+
+    match selected_file {
+        Some(file) => println!("{}", file.content),
+        None => println!("File not found."),
     }
 }
+
