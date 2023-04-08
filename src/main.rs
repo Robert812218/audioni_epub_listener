fa55ba23ebd51dadff8be8f207ff306a39be2627
Robert812218 <robert812@shaw.ca>
2023-04-08 10:15:30 -0600

    update main again

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,45 @@
-use std::io;
-use std::io::Read;
 use std::fs;
+use std::io::{BufReader, Read};
+use std::path::Path;
 
-#[derive(Debug)]
-struct MyFiles {
-    file_names: Vec<String>,
+// Define a struct to hold the title and content of a text file
+struct TextFile {
+    title: String,
+    content: String,
 }
 
-fn main() -> std::io::Result<()> {
-    let mut my_files = MyFiles {
-        file_names: Vec::new(),
-    };
+// Define a function to read the contents of a file given its path
+fn read_file(path: &Path) -> Result<String, std::io::Error> {
+    let file = fs::File::open(path)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut content = String::new();
+    buf_reader.read_to_string(&mut content)?;
+    Ok(content)
+}
 
-    let paths = fs::read_dir("src/library/")?;
-    
-    for path in paths {
-        let file_name = path?.file_name().into_string().unwrap();
-        my_files.file_names.push(file_name);
+fn main() {
+    let folder_path = Path::new("src/library");
+    let mut text_files = Vec::new();
+
+    for entry in fs::read_dir(folder_path).unwrap() {
+        let entry = entry.unwrap();
+        let file_path = entry.path();
+        let file_name = entry.file_name().into_string().unwrap();
+
+        if file_path.is_file() {
+            if let Ok(content) = read_file(&file_path) {
+                let text_file = TextFile {
+                    title: file_name,
+                    content: content,
+                };
+                text_files.push(text_file);
+            }
+        }
     }
-    
-    my_files.file_names.sort();
 
-    println!("{:?}", my_files);
-
-    Ok(())
+    // Do something with the text_files vector
+    for text_file in text_files {
+        println!("Title: {}", text_file.title);
+        println!("Content: {}", text_file.content);
+    }
 }
