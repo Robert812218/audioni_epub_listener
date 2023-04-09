f61036db10c5227bf7d92564305aaa3b500fe9e8
Robert812218 <robert812@shaw.ca>
2023-04-09 12:39:37 -0600

    set up database

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+use postgres::{Client, NoTls};
+use std::env;
+
 mod library;
 
-fn main() {
+fn main() -> Result<(), postgres::Error> {
+    let password = env::var("audioni_DB_PASSWORD")
+        .expect("auioni_DB_PASSWORD environment variable not set");
+    let conn_string = "postgresql://audioni_user:audioni777@localhost/audioni";
+    let client = Client::connect(&conn_string, NoTls)?;
+
     let text_files = library::create_library();
     library::display_library(&text_files);
+
+    Ok(())
 }
+
+   //  let text_files = library::create_library();
+   //
+    // library::display_library(&text_files);
